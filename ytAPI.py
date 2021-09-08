3b21a2e41fb0b3b8810cce3cc8cadc7ebfaf061c
M4rieH <marie.s.hannestad@gmail.com>
2021-09-08 14:33:32 +0200

    Ryddet code for YTAPI

--- ytAPI.py
+++ ytAPI.py
@@ -1,22 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Sep  7 08:03:50 2021
+Created on Wed Sep  8 14:31:59 2021
 
 @author: Marie
 """
-
 from googleapiclient.discovery import build
-import grequests
+
+# Get data from Youtube API
 
 api_key = 'AIzaSyB7PXfRpIlMKg7msCnCx0UUAP5Xl86ljvw'
 
 youtube = build('youtube','v3',developerKey=(api_key))
-request = youtube.videos().list(part = 'contentDetails', id='th5_9woFJmk')
-response =request.execute()
-print(response)
+def request(iden):
+    request = youtube.videos().list(part = 'contentDetails', id={iden})
+    # response =request.execute()
+    # return response
 
-url = https://date.nager.at/api/v3/publicholidays/2017/AT
-GET = /api/v3/PublicHolidays/{Year}/{CountryCode}
-
-rs = (grequests.get(u) for c in urls)
-grequests.map(rs)
+request_test = request('SbOwzAl9ZfQ')
+print(request_test)
